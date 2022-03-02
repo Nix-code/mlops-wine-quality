f3a4b07b581352a39236df3a396c3f4fdbc2c928
Nix-code <nishantbanjade07@gmail.com>
2022-03-02 17:11:37 +0530

    pytest example

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,19 @@
 
 # writing test in function name is compulsion to let it detect
 # test 
-def test_generic():
-    a = 2
-    b = 2
 
-    assert a==b
+# test if the user inputs the value out of the range
+
+
+import pytest
+class NotInRange(Exception):
+    def __init__(self,message="value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+def test_generic():
+    a = 5
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange
+   #passed as of now
